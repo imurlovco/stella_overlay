34b550400761ed98ac84e577c9fc31df12fa8b63
HaeengIn <imurlovco@gmail.com>
2026-03-02 22:30:34 +0900

    Add Tesseract-OCR PATH

--- core/ocr_engine.py
+++ core/ocr_engine.py
@@ -1,6 +1,16 @@
-import pytesseract
-import cv2
+import pytesseract, cv2, sys, os
 
 def extract_text(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return pytesseract.image_to_string(gray, lang="kor")
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS  # PyInstaller 임시폴더
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+pytesseract.pytesseract.tesseract_cmd = resource_path(
+    "external/Tesseract-OCR/tesseract.exe"
+)
